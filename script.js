40492d232e5644b9ecaa747483fec2f3f52d90a6
Aaron Torres <aarontorres@Aarons-MBP.lan>
2023-10-02 23:32:33 -0700

    js and html

---
+++ script.js
@@ -0,0 +1,64 @@
+let questions = [
+    {
+    qnumber: 1,
+    question: "Which of the following keywords is used to define a variable in JS",
+    answer: "A and B",
+    options: [
+      "var",
+      "let",
+      "A and B",
+      "none of the above"
+    ]
+  },
+    {
+    qnumber: 2,
+    question: "How can a datatype be declared to be a constant type?",
+    answer: "const",
+    options: [
+      "const",
+      "var",
+      "let",
+      "constant"
+    ]
+  },
+    {
+    qnumber: 3,
+    question: "What keyword is used to check whether a given property is valid or not?",
+    answer: "in",
+    options: [
+      "in",
+      "is in",
+      "exists",
+      "lies"
+    ]
+  },
+    {
+    qnumber: 4,
+    question: "When an operators value is NULL, the typeof returned by the unary operator is:",
+    answer: "Object",
+    options: [
+      "Boolean",
+      "Undefined",
+      "Object",
+      "Integer"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following are closures in Javascript?",
+    answer: "all of the above",
+    options: [
+      "variables",
+      "functions",
+      "objectts",
+      "all of the above"
+    ]
+  },
+  
+
+];
+
+var startButton = document.querySelector(".startButton button");
+var quizInfo = document.querySelector(".quizInfo");
+var quizCard = document.querySelector(".quizCard");
+var answerList = document.querySelector(".answerList")
